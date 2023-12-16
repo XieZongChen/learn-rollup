e7f2d18d6895db83110147fc9eab419b7e834bef
谢宗辰 <xiezongchen@xiezongchendeMacBook-Pro.local>
2023-12-16 15:18:32 +0800

    feat: rollup 代码拆分

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,10 @@ export default {
   input: 'src/index.js', // 入口文件路径
   output: {
     // 输出的相关配置
-    file: 'lib/bundle.js', // 输出文件名
-    format: 'iife', // 输出格式
+    // file: 'lib/bundle.js', // 输出文件名
+    // format: 'iife', // 输出格式
+    dir: 'lib',
+    format: 'amd'
   },
   plugins: [json(), resolve(), commonjs()],
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-// 导入模块成员
-import { camelCase } from 'lodash-es';
-import { log } from './logger';
-import messages from './messages';
-import { name, version } from '../package.json';
-import cjs from './cjs-module';
+// // 导入模块成员
+// import { camelCase } from 'lodash-es';
+// import { log } from './logger';
+// import messages from './messages';
+// import { name, version } from '../package.json';
+// import cjs from './cjs-module';
 
-// 使用模块成员
-const msg = messages.hi;
+// // 使用模块成员
+// const msg = messages.hi;
 
-log(msg);
+// log(msg);
 
-log(name);
+// log(name);
 
-log(version);
+// log(version);
 
-log(camelCase('hello word'));
+// log(camelCase('hello word'));
 
-log(cjs);
+// log(cjs);
+
+import('./logger').then(({ log }) => {
+  log('code splitting~');
+});
